bb3557a701c359fbb7556f0294b558acf286089b
JJJ-maker <1776795871@qq.com>
2020-04-09 16:30:47 +0800

    feat:trans str to json test

---
+++ story/src/main/java/com/hackweek/story/Bean/ResultJson.java
@@ -0,0 +1,28 @@
+package com.hackweek.story.Bean;
+
+import com.alibaba.fastjson.JSONArray;
+
+import java.util.Map;
+
+public class ResultJson {
+        private int id;
+        private String name;
+        private String description;
+        private Map<String, JSONArray> map;
+
+        public int getId() { return id; }
+
+        public void setId(int id) { this.id = id; }
+
+        public String getName() { return name; }
+
+        public void setName(String name) { this.name = name; }
+
+        public String getDescription() { return description; }
+
+        public void setDescription(String description) { this.description = description; }
+
+        public Map<String, JSONArray> getMap() { return map; }
+
+        public void setMap(Map<String, JSONArray> map) { this.map = map; }
+}
--- story/src/main/java/com/hackweek/story/Play/GamePlay.java
+++ story/src/main/java/com/hackweek/story/Play/GamePlay.java
@@ -1,5 +1,9 @@
 package com.hackweek.story.Play;
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONArray;
+import com.hackweek.story.Bean.ResultJson;
+import com.hackweek.story.Bean.event;
 import com.hackweek.story.Bean.gamePlayer;
 import com.hackweek.story.Repository.eventRepository;
 import com.hackweek.story.Repository.playerRepository;
@@ -7,6 +11,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
 
 @RestController
 public class GamePlay {
@@ -51,5 +60,27 @@ public class GamePlay {
     @GetMapping("/gameover")
     public Object gameOver(){
         return eventRepository.findeventOver();
+    }
+
+
+    @GetMapping("/test")
+    public Object test() {
+        List<event> events = eventRepository.findeventsInHouse();
+
+        public ResultJson{List<ResultJson> resultJsons = new LinkedList<>();
+        for (int i = 0; i < 6; i++) {
+            String str = events.get(i).getOptions();
+            JSONArray jsonArray = JSON.parseArray(str);
+            Map<String, JSONArray> map = new HashMap<>();
+            map.put("option", jsonArray);
+            ResultJson json = new ResultJson();
+            json.setId(events.get(i).getId());
+            json.setName(events.get(i).getName());
+            json.setDescription(events.get(i).getDescription());
+            json.setMap(map);
+            resultJsons.add(json);
+        }
+        return resultJsons;
+    }
     }
 }
